2b3cff41f16e3c3276d7e5d5f423c55983424506
Mario <tafoya1897@gmail.com>
2026-02-18 21:25:37 -0800

    Replace HttpResponse with render to serve the login page in index view

--- login/views.py
+++ login/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the " \
-    "Login Index.")
+    return render(request, "login/login.html")
